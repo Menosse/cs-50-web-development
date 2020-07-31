66b1fa998c30f5dc1d9ca7ba5ed75f38dd4aba28
Menosse <famcsn2@gmail.com>
2020-07-31 19:05:47 -0300

    project 1

--- project-1/encyclopedia/views.py
+++ project-1/encyclopedia/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+def title(request, title):
+    return render(request, "encyclopedia/title.html",{
+        "title": markdown2.markdown(util.get_entry(title))
     })
 
