9dee22155f2d002ef1d44499fc03beaae97c0e53
Menosse <famcsn2@gmail.com>
2020-08-20 16:52:00 -0300

    singlepage

--- project-1/encyclopedia/urls.py
+++ project-1/encyclopedia/urls.py
@@ -4,10 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.showEntry, name="title"),
-    path("edit/<str:title>", views.editEntry, name="editentry"),
-    path("random", views.randomEntry, name="random"),
-    path("newentry", views.newEntry, name="newentry"),
-    path("search", views.search, name="search"),
+    path("sections/<int:num>", views.section, name="title"),
 
 ]
