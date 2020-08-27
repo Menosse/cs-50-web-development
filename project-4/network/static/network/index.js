fea50ded1e0e5975c03efe4a4494a03e40666f8a
Menosse <famcsn2@gmail.com>
2020-08-27 11:35:57 -0300

    project-4

--- project-4/network/static/network/index.js
+++ project-4/network/static/network/index.js
@@ -1,22 +1,58 @@
 // When DOM Loads, add event listener
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#post-form').addEventListener('submit', new_post);
+    load_posts();
     });
 
 function new_post(){
     event.preventDefault();
-    fetch('/compose', {
-        method: 'POST',
-        body: JSON.stringify({
-            post_body: document.querySelector('#post-body').value,
+    if(document.querySelector('#post-body').value === ''){
+        console.log("Post is empty")
+    }else{
+        fetch('/compose', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_body: document.querySelector('#post-body').value,
+            })
         })
-    })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            document.querySelector('#post-body').value = '';
+        })
+        .catch(error => {
+            console.log("Error", error);
+        })
+    }
+}
+
+function load_posts(){
+    fetch('/posts')
     .then(response => response.json())
-    .then(result => {
-        console.log(result);
-        document.querySelector('#post-body').value = '';
+    .then(result =>{
+        result.forEach(post => {
+            //Create user div
+            const user_div = document.createElement('div');
+            user_div.className = "user_div col-sm-2";
+            user_div.innerHTML = ` ${post.user}`;
+            
+            //Create post body col
+            const post_div = document.createElement('div');
+            post_div.innerHTML = `${post.body} ${post.num_likes}`;
+            post_div.className = "post_div col-sm";
+            
+            //Create new Row
+            const row_div = document.createElement('div');
+            row_div.className = "row";
+            row_div.appendChild(user_div)
+            row_div.appendChild(post_div)
+            
+            //Append to the DOM
+            document.querySelector('#posts-view').append(row_div);
+            //document.querySelector('#posts-view').append(post_div);
+        });
     })
-    .catch(error => {
-        console.log("Error", error);
+    .catch(error =>{
+        console.log("Error", error)
     })
 }
