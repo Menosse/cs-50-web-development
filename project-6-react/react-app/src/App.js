f605f765dcc2a09d4d8883b01322bc921343b358
Menosse <36850961+Menosse@users.noreply.github.com>
2021-01-09 17:37:33 +0900

    navbar

--- project-6-react/react-app/src/App.js
+++ project-6-react/react-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import GlobalStyle from './globalStyles';
 
 function App(){
     return(
         <>
-        <h1>AAAAAAHHHHHH</h1>
+        <GlobalStyle />
+        <Navbar />
         </>
     );
 }
